d072660003d6d23faeca320b169810d7ca232311
MALIX ABDUL AZIS <132062295+poomx17@users.noreply.github.com>
2023-10-09 08:21:54 +0700

    Add files via upload

---
+++ index.ios.js
@@ -0,0 +1,6 @@
+'use strict';
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+
+import App from './app/container/';
+AppRegistry.registerComponent('Pokemon', () => App);
